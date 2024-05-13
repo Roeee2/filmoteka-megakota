ed8a262d3199e17530ac05f8fe9bd753df8ad910
Joanna <joanna_pinczuk@hotmail.com>
2024-05-13 23:12:28 +0200

    node updates

--- src/js/popular-movies-home.js
+++ src/js/popular-movies-home.js
@@ -1,6 +1,4 @@
 import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import axios from 'axios';
 
 export async function popularMovies(page, gallery, btnNextPage) {}
