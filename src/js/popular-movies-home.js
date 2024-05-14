a3b0a2812c8a50d72ce4ae7602c46737b0aada8b
Joanna <joanna_pinczuk@hotmail.com>
2024-05-14 17:12:19 +0200

    popular movies function

--- src/js/popular-movies-home.js
+++ src/js/popular-movies-home.js
@@ -1,4 +1,15 @@
 import Notiflix from 'notiflix';
 import axios from 'axios';
+const apiKey = '91f5af2219e63824428db203e9d0f8bf';
 
-export async function popularMovies(page, gallery, btnNextPage) {}
+export async function popularMovies() {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`
+    );
+    return response.data;
+  } catch (error) {
+    Notiflix.Notify.failure('Error while loading the movies', error);
+    return null;
+  }
+}
