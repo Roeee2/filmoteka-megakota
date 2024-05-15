c4344d20d309ddee9aaa0a4617aa699aa6b5ce16
Tavsanka <domnikan@gmail.com>
2024-05-15 23:32:19 +0200

    updated

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,7 +2,7 @@
 const filmData = {
   cover: '../images/film@1x.jpg',
   title: 'A FISTFUL OF LEAD',
-  votes: '7.3 / 1260',
+  votes: '7.3  1260',
   popularity: '100.2',
   originalTitle: 'A FISTFUL OF LEAD',
   genre: 'Western',
@@ -46,7 +46,20 @@ document.addEventListener('DOMContentLoaded', () => {
   function updateModalContent() {
     document.getElementById('cover-image').src = filmData.cover;
     document.getElementById('movie-title').innerText = filmData.title;
-    document.getElementById('film-votes').innerText = filmData.votes;
+
+    const votesElement = document.getElementById('film-votes');
+    const votesSecondaryElement = document.getElementById(
+      'film-votes-secondary'
+    );
+
+    const votesInfo = filmData.votes.split(' ');
+    votesElement.innerText = votesInfo[0]; // Ustaw głosy (7.3)
+    votesSecondaryElement.innerText = votesInfo[1] || 'N/A'; // Ustaw liczbę (1260)
+
+    // Stylizacja informacji o głosach
+    votesElement.classList.add('film-votes');
+    votesSecondaryElement.classList.add('film-votes-secondary');
+
     document.getElementById('film-popularity').innerText = filmData.popularity;
     document.getElementById('film-original-title').innerText =
       filmData.originalTitle;
