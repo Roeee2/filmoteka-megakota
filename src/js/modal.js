af92de2c3ad7f42d29bb4e3e03aa71412e2568f4
Roeee2 <122810145+Roeee2@users.noreply.github.com>
2024-05-16 15:20:03 +0100

    cos sie mega odwalilo - help

--- src/js/modal.js
+++ src/js/modal.js
@@ -8,26 +8,47 @@ const filmData = {
   description:
     "ABOUT  Four of the West's most infamous outlaws assemble to steal a huge stash of gold from the most corrupt settlement of the gold rush towns. But not all goes to plan one is killed and the other three escapes with bags of gold hide out in the abandoned gold mine where they happen across another gang of three – who themselves were planning to hit the very same bank! As tensions rise, things go from bad to worse as they realise the bags of gold are filled with lead... they've been double crossed – but by who and how?",
 };
+
+const modal = document.getElementById('filmModal');
+const galleryList = document.querySelector('ul.gallery-cards');
+
 document.addEventListener('DOMContentLoaded', () => {
   const openModal = () => {
-    const modal = document.getElementById('filmModal');
     if (modal) {
       modal.classList.remove('is-hidden');
       updateModalContent();
+
+      // Add event listener to close modal when clicking outside of it
+      const backdrop = document.querySelector('.backdrop');
+      backdrop.addEventListener('click', closeModalOutside);
     }
   };
 
   const closeModal = () => {
-    const modal = document.getElementById('filmModal');
     if (modal) {
       modal.classList.add('is-hidden');
+
+      // Remove event listener from backdrop to prevent further clicks from closing the modal
+      const backdrop = document.querySelector('.backdrop');
+      backdrop.removeEventListener('click', closeModalOutside);
     }
   };
 
-  const filmCard = document.getElementById('film-card');
-  if (filmCard) {
+  // Function to close modal when clicking outside of it
+  const closeModalOutside = event => {
+    const modalContent = document.querySelector('.modal-content');
+    if (!modalContent.contains(event.target)) {
+      closeModal();
+    }
+  };
+
+  galleryList.addEventListener('click', openModal);
+
+  // Pobierz wszystkie elementy li z klasą film-card
+  const filmCards = document.querySelectorAll('.film-card');
+  filmCards.forEach(filmCard => {
     filmCard.onclick = openModal;
-  }
+  });
 
   const closeButton = document.querySelector('.modal-close-button');
   if (closeButton) {
