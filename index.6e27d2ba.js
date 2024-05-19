4e16bd8d45038cf302c56f9e9c50c42bd6490e33
Roeee2 <122810145+Roeee2@users.noreply.github.com>
2024-05-19 15:38:54 +0000

    Deploying to gh-pages from @ Roeee2/filmoteka-megakota@8e12611a5a84c3b3795bc7b79895d7035aac4e86 🚀

---
+++ index.6e27d2ba.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{let e=document.querySelector(".header-menu-list a:last-child"),t=document.querySelector(".head-bg"),l=document.querySelector(".head-lib-bg"),n=!1;e.addEventListener("click",d=>{d.preventDefault(),n?(e.textContent="LOG IN",t.style.display="block",l.style.display="none"):(e.textContent="LOG OUT",t.style.display="none",l.style.display="block"),n=!n})});
+//# sourceMappingURL=index.6e27d2ba.js.map
