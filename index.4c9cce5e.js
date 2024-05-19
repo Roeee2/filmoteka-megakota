32a9642ccc7127f94913f1f85b74bff0349707af
Roeee2 <122810145+Roeee2@users.noreply.github.com>
2024-05-19 23:09:04 +0000

    Deploying to gh-pages from @ Roeee2/filmoteka-megakota@2117e0d1de5c10af03ca50f82b6e4500de48912a 🚀

---
+++ index.4c9cce5e.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("signup-menu"),d=document.getElementById("signupModal"),n=document.getElementById("signupModalCloseButton");e.addEventListener("click",()=>{d.classList.remove("is-hidden")}),n.addEventListener("click",()=>{d.classList.add("is-hidden")});
+//# sourceMappingURL=index.4c9cce5e.js.map
