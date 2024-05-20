7f42a2a18b0d1a42340f7ddbd7ae3795eb9fec8a
MDProjectMT <md.monikatajchert@gmail.com>
2024-05-20 16:16:55 +0200

    Watched

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
+import { doc } from 'firebase/firestore';
 import { popularMovies } from './popular-movies-home';
+import { displayFromLocalStorage } from './common';
 
 const gallery = document.querySelector('.gallery-cards');
 let page = 1;
@@ -6,5 +8,12 @@ global.popularMovies = popularMovies;
 
 document.addEventListener('DOMContentLoaded', () => {
   popularMovies(1);
-  // getDataAndCreatePagination();
+  const watchedButton = document.querySelector('#btn-watched');
+  watchedButton.addEventListener('click', ev => {
+    displayFromLocalStorage('watched.');
+  });
+  const queueButton = document.querySelector('#btn-queue');
+  queueButton.addEventListener('click', ev => {
+    displayFromLocalStorage('queue.');
+  });
 });
