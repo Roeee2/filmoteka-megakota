34351286e07d44a0fa0da8454caa75ba3b814132
Roeee2 <122810145+Roeee2@users.noreply.github.com>
2024-05-20 21:04:33 +0100

    logon test after file name change

---
+++ src/js/logon.js
@@ -0,0 +1,11 @@
+const loginMenuButton = document.getElementById('login-menu');
+const loginModal = document.getElementById('loginModal');
+const loginModalCloseButton = document.getElementById('loginModalCloseButton');
+
+loginMenuButton.addEventListener('click', () => {
+  loginModal.classList.remove('is-hidden');
+});
+
+loginModalCloseButton.addEventListener('click', () => {
+  loginModal.classList.add('is-hidden');
+});
