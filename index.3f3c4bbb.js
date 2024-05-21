ef238a17f2dbf123bd3b1a7b3c0755df1b17d1df
Roeee2 <122810145+Roeee2@users.noreply.github.com>
2024-05-21 15:22:54 +0000

    Deploying to gh-pages from @ Roeee2/filmoteka-megakota@533423cd905d2449e710f592e615f95b34fc6b75 🚀

---
+++ index.3f3c4bbb.js
@@ -0,0 +1,2 @@
+!function(){let e=document.querySelector("footer-link"),t=document.querySelector("#studens-name-modal"),d=document.querySelector("students-close-button");e.addEventListener("click",()=>{t.classList.remove("is-hidden")}),d.addEventListener("click",()=>{t.classList.add("is-hidden")})}();
+//# sourceMappingURL=index.3f3c4bbb.js.map
---
+++ index.597881f9.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("footer-link"),t=document.querySelector("#studens-name-modal"),d=document.querySelector("students-close-button");e.addEventListener("click",()=>{t.classList.remove("is-hidden")}),d.addEventListener("click",()=>{t.classList.add("is-hidden")});
+//# sourceMappingURL=index.597881f9.js.map
